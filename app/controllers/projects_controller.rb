0d803cead6c1220ccbd1db418d181078507719aa
Qi Wu <qi@startyoshi.com>
2018-03-11 22:59:55 -0700

    Integrate new and create actions for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  def index
+    @project_items = Project.all
+  end
+
+  def new
+    @project_item = Project.new
+  end
+
+  def create
+    @project_item = Project.new(project_params)
+
+    respond_to do |format|
+      if @project_item.save
+        format.html { redirect_to projects_path, notice: 'Project was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(
+        :title,
+        :subtitle,
+        :body,
+      )
+    end
 end
