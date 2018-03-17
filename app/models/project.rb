62020bee6cce178592110da77deb4e6f9707a9cc
Qi Wu <qi@startyoshi.com>
2018-03-17 15:08:45 -0700

    implement image defaults

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_default
+
+  def set_default
+    self.main_image ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end
 end
