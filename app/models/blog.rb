a1f8f06be8570e444bfa767687448bd68b408e2b
Qi Wu <qi@startyoshi.com>
2018-03-17 12:24:18 -0700

    implement validations for blogs, skills, and project items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :body
+
   enum status: {
     draft: 0,
     published: 1,
