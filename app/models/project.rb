cad7368d1671ba4106ff1f1efb86ab60fb84e726
Qi Wu <qi@startyoshi.com>
2018-03-17 15:36:07 -0700

    implement concern for managing data

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,12 @@
 class Project < ApplicationRecord
+  includes Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   after_initialize :set_default
 
   def set_default
-    self.main_image ||= "https://placehold.it/600x400"
-    self.thumb_image ||= "https://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
   end
 end
