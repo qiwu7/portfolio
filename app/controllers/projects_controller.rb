2a0eae31c3290ddbb399b717ef7b87eded8afb68
Qi Wu <qi@startyoshi.com>
2018-05-05 22:16:56 -0700

    Implement authorization for controller and view of drag and drop

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project_item, only: [:edit, :update, :show, :destroy]
   layout "project"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :edit, :update]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :edit, :update, :sort]}, site_admin: :all
 
   def index
     @project_items = Project.by_position
