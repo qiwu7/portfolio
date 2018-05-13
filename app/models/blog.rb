107b6b766a80706ebf5d2a6bb3eda843394382be
Qi Wu <qi@startyoshi.com>
2018-05-13 09:54:03 -0700

    Require topic on a blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,10 +2,10 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  belongs_to :topic, optional: true
+  belongs_to :topic
   has_many :comments, dependent: :destroy
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic
 
   enum status: {
     draft: 0,
